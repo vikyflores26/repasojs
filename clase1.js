1877d8ef195a764fedc34e6f5ba9468b36fa1a2f
alumno <alumno@Instituto.local>
2023-09-11 10:50:57 -0300

    cambios

---
+++ clase1.js
@@ -0,0 +1,18 @@
+function estaAprobado(notaexamen)
+{
+    let estado 
+    if (notaexamen >=6)
+
+}
+else{
+    estado = 'desaprobaste, segui estudiando'
+}
+return estado
+}
+    let si clarisellevamaterias = false
+    if (clari se lleva materias = true)
+{
+    alert( ' sr va a diciembre ')
+}else{
+    alert('la dejan juntarse')
+}
