a8e61eb924f0d717d0cc2750babcec406dfe78fb
alumno <alumno@Instituto.local>
2023-09-25 10:56:27 -0300

    cambios

---
+++ claseCondicional.js
@@ -0,0 +1,17 @@
+let  (Mamaestaencasa = true )
+if (mamaestaencasa == false)
+{
+    alert('voy al centro caminando')
+    
+}else{
+alert('me lleva en auto')
+
+let examenAlumno = Number(prompt( 'nota'))
+if ( examenAlumno >=6)
+{
+    alert('estas aprobado')
+
+}else{
+    alert('estas desaprobado')
+}
+
