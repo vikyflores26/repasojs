bc70b5768c4e8b0dc65a17ac81c2e1d9ef167050
alumno <alumno@Instituto.local>
2023-10-20 09:30:45 -0300

    cambios

--- claseCondicional.js
+++ claseCondicional.js
@@ -25,3 +25,28 @@ if( edad >= 18) && ( trabajoformal == true ) && ( cantidadcasas > 0)
 }else{
 alert(' no podes entrar al sorteo')
 }
+/* 20/10/23*/
+
+function numeromayor (a,b){
+    if (a>b)
+}return a 
+}else{
+    return b
+}
+
+function positivonegativocero(num){
+    if(num > 0)
+{
+    return "positivo"
+}else if (num ==0)
+{else{
+    
+ return "negativo"
+}
+
+if ((num  > 25))&& (num<255)
+{
+return "esta en el ranjo de valores"
+}else{
+    return "no esta el ranjo de valores"
+}
